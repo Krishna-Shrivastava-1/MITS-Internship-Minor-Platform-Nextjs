a45d63366c21c0bab141b71122ae4e9eebb74f3d
Krishna-Shrivastava-1 <krshnshrvstv.gwl@gmail.com>
2025-10-06 00:08:08 +0530

    added unautorized access page ui and removed error of dashboard folder but again left uploading of completion certificate in internship data

--- app/unauthorized/page.jsx
+++ app/unauthorized/page.jsx
@@ -1,9 +1,17 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      UNauthorized Access
+    <div className='backbanForUnauthorized flex items-center  justify-center'>
+   <div className='text-center text-white  w-full'>
+       <h1 className='text-6xl font-bold'>Lost your way?</h1>
+      <p className='text-2xl'>Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
+     <Link href={'/login'}>
+      <Button className='cursor-pointer select-none bg-white text-black hover:bg-neutral-300 font-semibold'>Login Page</Button>
+     </Link>
+   </div>
     </div>
   )
 }
