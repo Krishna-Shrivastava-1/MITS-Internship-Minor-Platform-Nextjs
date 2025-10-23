930603191ab9374c0fd8fa70e408827cd622dd3c
Krishna-Shrivastava-1 <krshnshrvstv.gwl@gmail.com>
2025-10-23 19:37:05 +0530

    fixed ip rate limit login issue

--- rateLimit/redisRateLimiterInstance.js
+++ rateLimit/redisRateLimiterInstance.js
@@ -5,7 +5,7 @@ let redisClient;
 if (!global.redisClient) {
   // Create new connection only once (prevents multiple connections in dev hot reload)
   global.redisClient = new Redis(process.env.REDIS_URL, {
-    tls: process.env.REDIS_URL.startsWith("rediss://") ? {} : undefined, // for Upstash or secure Redis
+    tls: process.env.REDIS_URL?.startsWith("rediss://") ? {} : undefined, // for Upstash or secure Redis
   });
 }
 
